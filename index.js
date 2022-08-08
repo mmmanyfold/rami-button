ca032c959f72514c4b71e852e215377a0f84ed9e
Michelle Lim <elle@mmmanyfold.com>
2022-08-08 00:21:46 -0400

    add states via timeout

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const sync = () => {
-    console.log("sync")
+const publish = (element) => {
+    element.innerHTML = "Publishing...";
+    element.classList.add("loading");
+    setTimeout(() => {
+        element.innerHTML = "Published!";
+        setTimeout(() => {
+            element.innerHTML = "Publish";
+            element.classList.remove("loading");
+        }, 2000);
+    }, 5000);
 }
